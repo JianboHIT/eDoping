9759e8ed733e804c70f44785079157b84a9d8092
JianboHIT <zhjb1996@163.com>
2025-07-14 18:49:05 +0800

    update version to v0.4.0a1

--- src/edoping/misc.py
+++ src/edoping/misc.py
@@ -28,8 +28,8 @@ filedebug = '{}.debug'.format(__prog__)
 
 
 __author__ = 'Jianbo Zhu, Jingyu Li, Peng-Fei Liu'
-__version__ = '0.3.3'
-__date__ = '2025-08-13'
+__version__ = '0.4.0a1'
+__date__ = '2025-07-14'
 __description__ = 'Point Defect Formation Energy Calculation'
 
 __ref__ = """
